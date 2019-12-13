7a2ef214dc99f99b5848eba40b4fafe0d4a5886b
onteriowright <onteriowright@yahoo.com>
2019-12-13 14:12:30 -0600

    modified style

--- scripts/DailyJournalComponents/SavedJournalEntryComponent.js
+++ scripts/DailyJournalComponents/SavedJournalEntryComponent.js
@@ -1,10 +1,10 @@
 const SavedJournalEntryComponent = entry => {
   return `
     <section class="saved-notes">
-        <div><h4>Date:</h4> ${entry.date}</div>
-        <div><h4>Concept:</h4> ${entry.concepts}</div>
-        <div><h4>Entry:</h4> ${entry.textArea}</div>
-        <div><h4>Mood:</h4> ${entry.mood}</div>
+        <div class="saved"><h4>Date:</h4> ${entry.date}</div>
+        <div class="saved"><h4>Concept:</h4> ${entry.concepts}</div>
+        <div class="saved"><h4>Entry:</h4> ${entry.textArea}</div>
+        <div class="saved"><h4>Mood:</h4> ${entry.mood}</div>
     </section>
   `;
 };
